8013a42d1b3dd52379d1eb3201f5ab91f513a988
JBerat <berat.99@hotmail.com>
2022-07-01 23:17:46 +0300

    Functionality of equal signs are learned

---
+++ Egitimler/JavaScript Learn/JS Equal Sign/equalSign.js
@@ -0,0 +1,28 @@
+console.log(1 == 1)     /* return true */
+
+
+console.log(1 === 1)    /* return true */
+
+
+console.log(1 == "1")   /* return true */
+
+
+console.log(1 === "1")  /* return false */
+
+
+console.log(0 == false)  /* return true */
+
+
+console.log(0 === false)  /* return false */
+
+
+console.log(null === undefined) /* return true */
+
+
+console.log(null === undefined) /* return false */
+
+
+console.log(1 != "1")   /* return false */
+
+
+console.log(1 !== "1")  /* return true */
