39b1ce865dbca77013ea97da5678eab8d7429318
JBerat <berat.99@hotmail.com>
2022-07-03 14:33:04 +0300

    Redux example added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
 import { View } from 'react-native-web';
-import StateLearning from './React Learning/stateLearning';
+import StateLearning from "./React Learning/React UseState/stateLearning"
+import { useSelector, useDispatch } from "react-redux"
+import {increment, decrement} from "./ReduxFolder/actions"
 
 
 function App() {
+
+  const counter = useSelector(state => state.counter);
+  const isLogged = useSelector(state => state.isLogged);
+
+  const dispatch = useDispatch();
+
+
   return (
 
-        StateLearning()
+    //StateLearning()
+
+    <div className='App'>
+        <h1>Counter {counter} </h1>
+        <button onClick={() => dispatch(increment(5))}>+</button>
+        <button onClick={() => dispatch(decrement())}>-</button>
+
+        {isLogged ? <h3>Valuable Information  I Shouldn't see</h3> : ""}
+    </div>
+        
     
+        
     
   );
 }
